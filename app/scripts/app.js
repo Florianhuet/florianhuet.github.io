094811f2d83cf6d0a4d60d8ae0a98673018537dc
Florian Huet <florian.huet@epitech.eu>
2017-01-29 13:58:57 +0100

    test

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name florianhuetgithubioApp
+ * @description
+ * # florianhuetgithubioApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('florianhuetgithubioApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name florianhuetgithubioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the florianhuetgithubioApp
+ */
+angular.module('florianhuetgithubioApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
